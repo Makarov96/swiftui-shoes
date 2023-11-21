1e424005c4a213c82bd5cb01d1aaa3ec6200e86e
Makarov96 <estev97@gmail.com>
2023-11-21 10:16:18 -0600

    Initial Commit

---
+++ shoes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shoes
+//
+//  Created by Guerin Steven Colocho Chacon on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shoes/shoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoesApp.swift
+//  shoes
+//
+//  Created by Guerin Steven Colocho Chacon on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct shoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
